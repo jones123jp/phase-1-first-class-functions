56c9fa8a0498fd02a6640815eceeb496154149ca
jones <pnduru6@gmail.com>
2024-12-14 18:20:14 +0300

    changes

--- index.js
+++ index.js
@@ -1,25 +1,20 @@
 function receivesAFunction(callback) {
-    //call the callback function 
+    // Call the callback function
     callback();
-}
-
-
-
-
+  }
+  
 function returnsANamedFunction() {
-    //Define and return a named function
-    function NamedFunction() {
-        console.log("I am a named Function");
+    function namedFunction() {
+        console.log("This is a named function.");
     }
     return namedFunction;
 }
 
 
 function returnsAnAnonymousFunction() {
-    //DEfine and return an anonymuos function
-    function Anonymuous() {
-        console.log("I am an anonymuos Function")
-    }
-    return anonymuousFunction;
-
-}
+    return function () {
+      return 'I am anonymous!';
+    };
+  }
+  
+  
